167b2d07f00cfe1fadd93f26fc902f00a791462e
lamyoung <>
2019-09-25 22:47:04 +0800

    摄像机实现背景滚动

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# cocos_creator_camera_demo
-
-[cocos creator 用摄像机实现局部缩放的效果](https://mp.weixin.qq.com/s?__biz=MzI1Nzk1MzExNw==&mid=2247483790&idx=1&sn=486ed7d67a35b8dc9efc960c27bd7a4d&chksm=ea0eddbcdd7954aad58a914f7898a57ae8d9780c7c211e6f94b5363a41d59dec1c9c2d6ddb18&token=2033009213&lang=zh_CN#rd)
-
-[预览](http://lamyoung.com/website/ccc_camera_demo/)
-
+# 摄像机实现背景滚动
 ![](./img/ccc-camera-preview.gif)
 
+---
+cocos creator v2.0.8
 ---
 微信公众号  
 ![](./img/qrcode.jpg)
--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -11,15 +11,6 @@ export default class Helloworld extends cc.Component {
     @property(cc.Node)
     node_icon: cc.Node = null;
 
-    @property(cc.Slider)
-    sl_scale: cc.Slider = null;
-
-    @property(cc.Slider)
-    sl_h: cc.Slider = null;
-
-    @property(cc.Slider)
-    sl_v: cc.Slider = null;
-
     onLoad() {
         const texture = new cc.RenderTexture();
         texture.initWithSize(this.sp_camera.node.width, this.sp_camera.node.height);
@@ -28,28 +19,14 @@ export default class Helloworld extends cc.Component {
         this.sp_camera.spriteFrame = spriteFrame;
         this.camera.targetTexture = texture;
 
-        this.node_icon.runAction(cc.repeatForever(cc.rotateBy(1, 100)));
-        this.onSliderChange(this.sl_scale);
-        this.onSliderChange(this.sl_h);
-        this.onSliderChange(this.sl_v);
+        this.node_icon.runAction(cc.repeatForever(cc.sequence(cc.moveBy(0.5, 0, 20), cc.moveBy(0.5, 0, -20))));
     }
 
-    private onSliderChange(slider?: cc.Slider) {
-        switch (slider) {
-            case this.sl_scale: {
-                const zoomRatio = this.sl_scale.progress * 2;
-                this.camera.zoomRatio = zoomRatio;
-                break;
-            }
-            case this.sl_h: {
-                this.camera.node.x = (this.sl_h.progress - 0.5) * (this.node.width);
-                break;
-            }
-            case this.sl_v: {
-                this.camera.node.y = (this.sl_v.progress - 0.5) * (this.node.height)
-                break;
-            }
+    update() {
+        let newX = this.camera.node.x + 2;
+        if (newX > this.node.width / 2) {
+            newX = -this.node.width / 2;
         }
-
+        this.camera.node.x = newX;
     }
 }
