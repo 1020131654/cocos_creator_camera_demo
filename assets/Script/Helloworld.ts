741e6feaa6c690e6f63230753ce21f1d031941c5
lamyoung <>
2019-09-26 19:04:34 +0800

    init shadwo

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -5,51 +5,38 @@ export default class Helloworld extends cc.Component {
     @property(cc.Camera)
     camera: cc.Camera = null;
 
-    @property(cc.Sprite)
-    sp_camera: cc.Sprite = null;
+    @property([cc.Sprite])
+    sp_cameras: cc.Sprite[] = [];
 
     @property(cc.Node)
     node_icon: cc.Node = null;
 
-    @property(cc.Slider)
-    sl_scale: cc.Slider = null;
-
-    @property(cc.Slider)
-    sl_h: cc.Slider = null;
-
-    @property(cc.Slider)
-    sl_v: cc.Slider = null;
-
     onLoad() {
         const texture = new cc.RenderTexture();
-        texture.initWithSize(this.sp_camera.node.width, this.sp_camera.node.height);
+        texture.initWithSize(this.sp_cameras[0].node.width, this.sp_cameras[0].node.height);
         const spriteFrame = new cc.SpriteFrame();
         spriteFrame.setTexture(texture);
-        this.sp_camera.spriteFrame = spriteFrame;
         this.camera.targetTexture = texture;
-
-        this.node_icon.runAction(cc.repeatForever(cc.rotateBy(1, 100)));
-        this.onSliderChange(this.sl_scale);
-        this.onSliderChange(this.sl_h);
-        this.onSliderChange(this.sl_v);
+        this.sp_cameras.forEach((v) => {
+            v.spriteFrame = spriteFrame
+        })
+        // this.node_icon.runAction(cc.repeatForever(cc.rotateBy(1, 100)));
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, this.onNodeIconTouchMove, this);
+        this.schedule(this.shadowFollow, 0.1, cc.macro.REPEAT_FOREVER);
     }
 
-    private onSliderChange(slider?: cc.Slider) {
-        switch (slider) {
-            case this.sl_scale: {
-                const zoomRatio = this.sl_scale.progress * 2;
-                this.camera.zoomRatio = zoomRatio;
-                break;
+    private shadowFollow() {
+        this.sp_cameras.forEach((v, i) => {
+            const dis = Math.sqrt(Math.pow(v.node.x - this.node_icon.x, 2) + Math.pow(v.node.x - this.node_icon.y, 2));
+            if (dis > 0) {
+                v.node.stopAllActions();
+                v.node.runAction(cc.moveTo(i * 0.05 + 0.02, this.node_icon.x, this.node_icon.y));
             }
-            case this.sl_h: {
-                this.camera.node.x = (this.sl_h.progress - 0.5) * (this.node.width);
-                break;
-            }
-            case this.sl_v: {
-                this.camera.node.y = (this.sl_v.progress - 0.5) * (this.node.height)
-                break;
-            }
-        }
+        })
+    }
 
+    private onNodeIconTouchMove(evt: cc.Event.EventTouch) {
+        this.node_icon.x += evt.getDeltaX();
+        this.node_icon.y += evt.getDeltaY();
     }
 }
